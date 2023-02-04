d1111970e0a2bccc4b381a6f77de467512c5f2be
Jimmy Tan <jimmytan0424@gmail.com>
2023-02-04 10:51:26 -0800

    Optimization using async components

--- src/router.js
+++ src/router.js
@@ -7,7 +7,6 @@ import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
-
 import store from './store/index.js';
 
 const router = createRouter({
